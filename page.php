702a3269e7d065e8a9921aba71810c4291350b60
H3ndrik-Jan <46112431+H3ndrik-Jan@users.noreply.github.com>
2020-04-08 17:32:49 +0200

    Update page.php

--- page.php
+++ page.php
@@ -47,7 +47,7 @@ $limit = $_POST['+1'];
 }
 
 
-    $conn = mysqli_connect("localhost", "page", "page", "labpsu");
+    $conn = mysqli_connect("servername", "username", "password", "database");
     // Check connection
     if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
